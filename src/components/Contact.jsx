dafcbcddab1bc6dad9ed65e9e347d0e6c713c654
Davide <davidelaplaca92@gmail.com>
2023-11-18 19:09:37 -0800

    adjust media queries

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,9 +4,9 @@ import React from "react";
 
 export default function Contact() {
     return (
-        <div name="contact" className=" w-full h-screen bg-gradient-to-b from-gray-800 to-black p-4 text-white">
+        <div name="contact" className=" w-full h-screen  bg-gradient-to-b from-gray-800 to-black p-4 text-white">
             <div className=" flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
-                <div className=" pb-8">
+                <div className=" mt-48 sm:mt-0 pb-8 ">
                     <p className=" text-4xl font-bold inline border-b-4 border-gray-500">Contact</p>
                     <p className="py-6">Submit the form below to get in touch with me</p>
                 </div>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -66,7 +66,7 @@ export default function Experience() {
         className=" bg-gradient-to-b from-black to-gray-800 w-full h-screen"
         >
             <div className=" max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
-                <div>
+                <div className=" mt-48 sm:mt-0">
                     <p className=" text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Experience</p>
                     <p className=" py-6">Some of the technologies that I work with</p>
                 </div>

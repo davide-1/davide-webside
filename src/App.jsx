813c5217d7ccffb651cc759941c051d40cadbd7a
Davide <davidelaplaca92@gmail.com>
2025-09-12 18:12:56 -0700

    Update About section

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./components/Home"
 import Navbar from "./components/Navbar"
 // import Portfolio from "./components/Portfolio"
 import SocialLinks from "./components/SocialLinks"
-import React from 'react'
+
 
 function App() {
 
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -18,7 +18,10 @@ export default function Portfolio() {
   ];
 
   return (
-    <div className=" bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
+    <div 
+    name="portfolio"
+    className=" bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
       <div className=" max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className=" pb-8">
           <p className=" text-4xl font-bold inline border-b-4 border-gray-500">
@@ -27,7 +30,10 @@ export default function Portfolio() {
           <p className="py-6">Check some of my work</p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+        <div 
+        
+        className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0"
+        >
           {portfolios.map(({ id, src }) => (
             <div key={id} className=" shadow-md shadow-gray-600 rounded-lg">
               <img
